c698e843af7a5de3c94ccc43a7ebca536faa519d
Esteban Avila <cqasss@gmail.com>
2023-02-25 21:44:01 -0300

    ABM

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, request, redirect
+import os
+from flask import Flask, render_template, request, redirect, send_from_directory
+from flaskext.mysql import MySQL
+from datetime import datetime
 
 app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'crudnode'
+mysql.init_app(app)
 
 
 @app.route('/')
 def inicio():
     return render_template('sitio/index.html')
+
+
+@app.route('/img/<imagen>')
+def imagenes(imagen):
+    print(imagen)
+    return send_from_directory(os.path.join('templates/sitio/img'), imagen)
 
 
 @app.route('/libros')
@@ -30,12 +46,65 @@ def admin_login():
 
 @app.route('/admin/libros')
 def admin_libros():
-    return render_template('admin/libros.html')
+
+    con = mysql.connect()
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM libros')
+    libros_list = cursor.fetchall()
+    con.commit()
+    con.close()
+
+    return render_template('admin/libros.html', list_libros=libros_list)
 
 
 @app.route('/admin/libros/guardar', methods=['POST'])
 def admin_libros_guardar():
-    print(request.form['txtTitulo'])
+
+    _titulo = request.form['txtTitulo']
+    _Url = request.form['txtUrl']
+    _Imagen = request.files['txtImagen']
+
+    horaActual = datetime.now().strftime('%Y%H%M%S')
+    cortado = _Imagen.filename.split('.')
+
+    if _Imagen.filename != "":
+        nombreNuevo = cortado[0] + '_' + horaActual + '.' + cortado[1]
+        _Imagen.save('templates/sitio/img/' + nombreNuevo)
+
+    query = "INSERT INTO `libros`(`id`, `titulo`, `url`, `imagen`) VALUES (NULL,%s,%s,%s)"
+    datos = (_titulo, _Url, nombreNuevo)
+
+    con = mysql.connect()
+    cursor = con.cursor()
+    cursor.execute(query, datos)
+    con.commit()
+    con.close()
+
+    return redirect('/admin/libros')
+
+
+@app.route('/admin/libros/borrar', methods=['POST'])
+def admin_libros_borrar():
+
+    _id = request.form['txtId']
+
+    con = mysql.connect()
+    cursor = con.cursor()
+    cursor.execute('SELECT imagen FROM libros WHERE id=%s', (_id))
+    libro = cursor.fetchall()
+    con.commit()
+
+    if os.path.exists('templates/sitio/img/' + str(libro[0][0])):
+        os.unlink('templates/sitio/img/' + str(libro[0][0]))
+
+    query = "DELETE FROM libros WHERE id=%s"
+    datos = (_id)
+
+    cursor = con.cursor()
+    cursor.execute(query, datos)
+    con.commit()
+    con.close()
+
     return redirect('/admin/libros')
 
 
